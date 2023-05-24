c3bf4860a1b9434533b2619395a5d394ef808c7c
Shaman58 <vseslav.polockii@gmail.com>
2023-05-24 16:53:30 +0300

    add some endpoints, mappers and services

--- src/main/java/ru/erp/sfsb/model/Department.java
+++ src/main/java/ru/erp/sfsb/model/Department.java
@@ -1,14 +1,14 @@
 package ru.erp.sfsb.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
-
-import static jakarta.persistence.CascadeType.ALL;
 
 @Getter
 @Setter
