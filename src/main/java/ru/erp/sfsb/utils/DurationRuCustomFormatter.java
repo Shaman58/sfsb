39b7702aa21347b3d66f8b106a79f090a59db3dc
Shaman58 <vseslav.polockii@gmail.com>
2024-01-11 09:53:58 +0300

    v-0.1.7 add operation report total time

--- src/main/java/ru/erp/sfsb/utils/DurationRuCustomFormatter.java
+++ src/main/java/ru/erp/sfsb/utils/DurationRuCustomFormatter.java
@@ -1,13 +1,10 @@
 package ru.erp.sfsb.utils;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.util.Arrays;
 
 @Component
-@Slf4j
 public class DurationRuCustomFormatter {
 
     public String getRusTimeFormat(Duration duration) {
@@ -16,7 +13,6 @@ public class DurationRuCustomFormatter {
 
     public Duration getDurationFromString(String string) {
         var strings = string.split(" ");
-        log.info(Arrays.toString(strings));
         var hours = Duration.ofHours(Integer.parseInt(strings[0].split("ч.")[0]));
         var minutes = Duration.ofMinutes(Integer.parseInt(strings[1].split("м.")[0]));
         return hours.plus(minutes);
