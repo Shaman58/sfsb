f2ce3e6f8c70b8af99fda3cf3c88453c4b779a3d
ЛомакинДВ <ldv05@yandex.ru>
2024-07-09 08:34:36 +0300

    исправил баг с колонкой deleted  в native query

--- src/main/java/ru/erp/sfsb/repository/repos/FileEntityRepository.java
+++ src/main/java/ru/erp/sfsb/repository/repos/FileEntityRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface FileEntityRepository extends EntityRepository<File> {
 
-    @Query(value = "SELECT f.filename, f.link, f.id, f.created, f.updated, f.deleted, f.user_uuid FROM files f JOIN order_files off ON off.file_id = f.id WHERE off.order_id = :orderId", nativeQuery = true)
+    @Query(value = "SELECT f.filename, f.link, f.id, f.created, f.updated, f.deleted, f.user_uuid FROM files f JOIN order_files of ON of.file_id = f.id WHERE of.order_id = :orderId", nativeQuery = true)
     List<File> getFilesByOrderId(@Param("orderId") Long orderId);
 
     @Modifying
