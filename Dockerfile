42113b9a12ab435ccfee575ca73ce062941bee5a
ЛомакинДВ <ldv05@yandex.ru>
2024-07-23 14:18:16 +0300

    тестирование deploy/release-test 0.0.10

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-#FROM --platform=linux/amd64 openjdk:17.0.2-jdk-slim-buster
-FROM amazoncorretto:18-alpine
+FROM --platform=linux/amd64 openjdk:17.0.2-jdk-slim-buster
+#FROM amazoncorretto:18-alpine
 ARG JAR_FILE=target/sfsb-0.2.3-SNAPSHOT.jar
 WORKDIR /opt/app
 COPY ${JAR_FILE} app.jar
--- src/main/java/ru/erp/sfsb/SfsbApplication.java
+++ src/main/java/ru/erp/sfsb/SfsbApplication.java
@@ -2,10 +2,12 @@ package ru.erp.sfsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableCaching
 public class SfsbApplication {
 
     public static void main(String[] args) {
