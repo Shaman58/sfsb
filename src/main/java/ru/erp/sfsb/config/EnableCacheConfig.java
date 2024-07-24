dd1250804cc0e555bcf3a7a712f654dcee8c638d
ldv0001 <61231034+ldv0001@users.noreply.github.com>
2024-07-24 15:11:38 +0300

    добавил конфиг для кэширования (#4)

---
+++ src/main/java/ru/erp/sfsb/config/EnableCacheConfig.java
@@ -0,0 +1,9 @@
+package ru.erp.sfsb.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class EnableCacheConfig {
+}
