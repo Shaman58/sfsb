6ab89f57f6598d69ffaaff01fc0c9d5870d1f435
SergeyT <telegin665@mail.ru>
2024-07-30 11:21:30 +0300

    добавлена документация fix-4

--- src/main/java/ru/erp/sfsb/controller/MeasureToolController.java
+++ src/main/java/ru/erp/sfsb/controller/MeasureToolController.java
@@ -26,14 +26,14 @@ public class MeasureToolController {
     private final MeasureToolService measureToolService;
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Просмотреть инструмент измерения по ID")
+    @Operation(summary = "Просмотреть меритель по ID")
     @GetMapping("/{id}")
     public MeasureToolDto get(@PathVariable Long id) {
         return measureToolService.get(id);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Просмотреть все инструменты измерения")
+    @Operation(summary = "Просмотреть все мерители")
     @GetMapping()
     public List<MeasureToolDto> getAll(
             @RequestParam(value = "offset", defaultValue = "0") @Min(0) Integer offset,
@@ -43,7 +43,7 @@ public class MeasureToolController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Просмотреть все инструменты измерения")
+    @Operation(summary = "Просмотреть все мерители")
     @GetMapping("/page")
     public Page<MeasureToolDto> getAllInPage(@RequestParam(value = "offset", defaultValue = "0") @Min(0) Integer offset,
                                              @RequestParam(value = "limit", defaultValue = "20") @Min(1) @Max(100) Integer limit) {
@@ -51,14 +51,14 @@ public class MeasureToolController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "Добавить новый инструмент измерения")
+    @Operation(summary = "Добавить новый меритель")
     @PostMapping()
     public MeasureToolDto save(@RequestBody @Valid MeasureToolDto measureToolDto) {
         return measureToolService.save(measureToolDto);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Изменить инструмент измерения по ID")
+    @Operation(summary = "Изменить меритель по ID")
     @PutMapping("/{id}")
     public MeasureToolDto update(@RequestBody @Valid MeasureToolDto measureToolDto,
                                  @PathVariable @Min(1) @Max(Long.MAX_VALUE) Long id) {
@@ -67,7 +67,7 @@ public class MeasureToolController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Удалить инструмент измерения по ID")
+    @Operation(summary = "Удалить меритель по ID")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable @Min(1) @Max(Long.MAX_VALUE) Long id) {
         measureToolService.delete(id);
