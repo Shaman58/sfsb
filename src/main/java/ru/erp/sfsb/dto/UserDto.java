1eade4f265044400c16e99248bf30f00de759016
Shaman58 <vseslav.polockii@gmail.com>
2024-08-10 23:57:11 +0300

    Пофиксил кеш

--- src/main/java/ru/erp/sfsb/dto/UserDto.java
+++ src/main/java/ru/erp/sfsb/dto/UserDto.java
@@ -8,34 +8,36 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
 @NoArgsConstructor
 public class UserDto {
 
     private String id;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     @NotBlank(message = "Никнэйм не может быть пустым")
     private String username;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     @NotBlank(message = "Имя не может быть пустым")
     private String firstName;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     @NotBlank(message = "Фамилия не может быть пустой")
     private String lastName;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     private String email;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     private String password;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     @NotNull()
     private List<String> roles;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonInclude(NON_NULL)
     private String picture;
 }
